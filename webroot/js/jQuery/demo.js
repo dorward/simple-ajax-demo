fe49ba3446f6bd61082b001ed2f0a253444dddc9
David Dorward <david@dorward.me.uk>
2008-11-16 23:38:42 +0000

    Use the URI for type switching for the time being

--- webroot/js/jQuery/demo.js
+++ webroot/js/jQuery/demo.js
@@ -2,8 +2,8 @@ jQuery(document).ready(function (){
 	var conf = {
 		success    : function () { window.alert('OK'); },
 		error      : function () { window.alert('FAIL'); },
-		beforeSend : function (xhr) { alert(xhr); xhr.setRequestHeader("accept", "application/json;q=1,text/html;q=0"); },
-		url        : "/demo.pl"
+		url        : "/demo.pl?type=json",
+		dataType   : "json"
 	};
 	jQuery.ajax(conf);
 });
